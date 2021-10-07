e344a1e3d4587b349cc4a6767197bd7e001930f9
Dibas Dauliya <dibasdauliya@gmail.com>
2021-10-07 12:13:19 +0545

    adding blog posts in homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,36 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 import Layout from '../components/layout';
 
 export default function IndexPage() {
+  const data = useStaticQuery(graphql`
+    query MyQuery {
+      allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+        nodes {
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            description
+            title
+          }
+          id
+          slug
+        }
+      }
+    }
+  `);
+  const posts = data.allMdx.nodes;
   return (
     <Layout>
       <h1>Hello world</h1>
       <Link to="/about">About</Link>
+
+      <h2>Recent blog posts</h2>
+      {posts.map(({ id, slug, frontmatter }) => (
+        <li key={id}>
+          <Link to={slug}>{frontmatter.title}</Link>{' '}
+          <small>Posted at {frontmatter.date}</small>
+        </li>
+      ))}
     </Layout>
   );
 }
