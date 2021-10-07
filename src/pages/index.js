8f2d6240b67c96defbbc407c281ac12ce9ed2403
Dibas Dauliya <dibasdauliya@gmail.com>
2021-10-07 11:04:12 +0545

    use shared layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,12 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
-import { Seo } from '../components/seo';
+import Layout from '../components/layout';
 
 export default function IndexPage() {
-  //  && - xa vane and ?? - xaina vane
-  // const meta = data?.site?.siteMetadata ?? {};
-
   return (
-    <>
-      <Seo />
-      <main>
-        <h1>Hello world</h1>
-        <Link to="/about">About</Link>
-      </main>
-    </>
+    <Layout>
+      <h1>Hello world</h1>
+      <Link to="/about">About</Link>
+    </Layout>
   );
 }
